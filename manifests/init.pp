be1ed4816fcb34f3e84ef9da515f76e866665633
Joshua Hoblitt <josh@hoblitt.com>
2016-06-17 09:44:44 -0700

    linter fixes

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,17 +19,17 @@ class slack {
       $pkg = "${pkg_name}-2.0.6-amd64.deb"
       $provider = 'dpkg'
       $deps = [
-				'libxss1',
-				'gconf2',
-				'gconf-service',
-				'libgtk2.0-0',
-				'libnotify4',
-				'libxtst6',
-				'libnss3',
-				'gvfs-bin',
-				'xdg-utils',
-				'apt-transport-https',
-			]
+        'libxss1',
+        'gconf2',
+        'gconf-service',
+        'libgtk2.0-0',
+        'libnotify4',
+        'libxtst6',
+        'libnss3',
+        'gvfs-bin',
+        'xdg-utils',
+        'apt-transport-https',
+      ]
     }
     default: {
       fail("unsupported operatingsystem: ${::operatingsystem}")
@@ -38,19 +38,19 @@ class slack {
 
   $download_url = "https://downloads.slack-edge.com/linux_releases/${pkg}"
 
-	$path = '/opt/slack'
+  $path = '/opt/slack'
 
-	file { $path:
-		ensure => directory,
-	}
+  file { $path:
+    ensure => directory,
+  }
 
-	archive { $pkg:
-		source  => $download_url,
-		path    => "${path}/$pkg",
-		cleanup => false,
-		extract => false,
-		require => File[$path],
-	}
+  archive { $pkg:
+    source  => $download_url,
+    path    => "${path}/${pkg}",
+    cleanup => false,
+    extract => false,
+    require => File[$path],
+  }
 
   if $deps {
     ensure_packages($deps)
@@ -59,7 +59,7 @@ class slack {
 
   package { $pkg_name:
     ensure   => present,
-    source   => "${path}/$pkg",
+    source   => "${path}/${pkg}",
     provider => $provider,
     require  => Archive[$pkg],
   }
